251aa9703f719b04d82f7b442ec4f89e5b0922e1
freakeinstein <wow.dr.dre@gmail.com>
2022-03-17 17:01:05 +0530

    JS updates

--- src/alpine/main.js
+++ src/alpine/main.js
@@ -1,43 +1,153 @@
 console.log("Welcome to Aquila Network")
 
-window.onload = function () {
-    // initialize everything
-
-}
-
-// init alpine data stores
+// Alpine initialized succesfully
 document.addEventListener('alpine:init', () => {
+    // Initialize all Alpine datastructures (Alpine Store)
+    initURLParams()
     initAuthentication()
+    initProfileInformation()
+    initSearchInput()
     initSearchResults()
 })
 
+// Webpage is loaded and ready
+window.onload = function () {
+    // initialize url params on load
+    Alpine.store('urlParams').loadURLParams()
+    // initialize user by performing authentication on load
+    Alpine.store('authentication').authenticate()
+    Alpine.store('authentication').loadUserInfo()
+
+}
+
+function initURLParams() {
+    // Initialize by fetching anything that's available in the url parameters for later actions 
+    Alpine.store('urlParams', {
+        loadURLParams() {
+            // get params
+            const urlParams = new URLSearchParams(window.location.search)
+
+            // validate & assign params
+            // 1. query
+            queryText = urlParams.get("q")
+            if (queryText != null && queryText.trim() === "") {
+                queryText = null
+            }
+            this.queryText = queryText
+
+            // 2. share ID
+            shareID = urlParams.get("share")
+            if (shareID != null && shareID.trim() === "") {
+                shareID = null
+            }
+            this.shareID = shareID
+        },
+        shareID: null,
+        queryText: null
+    })
+}
+
 function initAuthentication() {
+    // Authenticate user by communicating with addon, fetch user profile & keep it
     Alpine.store('authentication', {
         authenticate() {
 
             // get secret key from addon
 
             // get authentication status from API
-            this.isAuthenticated = ! this.isAuthenticated
+            this.isAuthenticated = !this.isAuthenticated
         },
-        isAuthenticated: true
+        isAuthenticated: true,
+        loadUserInfo() {
+            // get user information from API
+        },
+        user: {
+            name: "Wei Dailey",
+            rating: 4,
+            avatar: "https://api.lorem.space/image/face?hash=92310",
+            description: "Wei Dailey does some cool stuff. A developer by day, shitposter by night, and a Bitcoiner for life.",
+            publicURL: "sd231sd2sdss412sd342s3s52s35fsdtgsdg"
+        }
+    })
+}
+
+function initProfileInformation() {
+    // Get profile information for any public profile given the ID
+    Alpine.store('profileInfo', {
+        get(shareID) {
+            // call API to get data for a profile with ShareID
+            
+            return {
+                name: "Wei Dailey",
+                rating: 4,
+                avatar: "https://api.lorem.space/image/face?hash=92310",
+                description: "Wei Dailey does some cool stuff. A developer by day, shitposter by night, and a Bitcoiner for life.",
+                publicURL: "sd231sd2sdss412sd342s3s52s35fsdtgsdg"
+            }
+        }
+    })
+}
+
+function initSearchInput() {
+    // get all search input configurations, parametrs, filters, query etc.
+    Alpine.store('searchInput', {
+        queryText: null
     })
 }
 
 function initSearchResults() {
-
+    // configure & cache search results for a personal search, public search & aggregated search separately
     Alpine.store('personalResults', {
         resett() {
-            this.results = [
-                {
-                    "title": "I'm a simple link title reset",
-                    "url": "https://google.com",
-                    "description": "If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?",
-                    "timestamp": 123456789,
-                    "author": "John Smith",
-                    "website": "medium.com"
-                }
-            ]
+            this.results = []
+        },
+        results: [
+            {
+                "title": "I'm a simple link title 1",
+                "url": "https://google.com",
+                "description": "If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?",
+                "timestamp": 123456789,
+                "author": "John Smith",
+                "website": "medium.com"
+            },
+            {
+                "title": "I'm a simple link title 2",
+                "url": "https://google.com",
+                "description": "If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?",
+                "timestamp": 123456789,
+                "author": "John Smith",
+                "website": "medium.com"
+            }
+        ]
+    })
+
+    Alpine.store('publicResults', {
+        resett() {
+            this.results = []
+        },
+        results: [
+            {
+                "title": "I'm a simple link title 1",
+                "url": "https://google.com",
+                "description": "If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?",
+                "timestamp": 123456789,
+                "author": "John Smith",
+                "website": "medium.com"
+            },
+            {
+                "title": "I'm a simple link title 2",
+                "url": "https://google.com",
+                "description": "If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?",
+                "timestamp": 123456789,
+                "author": "John Smith",
+                "website": "medium.com"
+            }
+        ]
+    })
+
+    Alpine.store('mixedResults', {
+        resett() {
+            this.results = []
         },
         results: [
             {
